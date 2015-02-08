3da9c95557f9c3b25fc202556d64868a6836454f
Gant <GantMan@gmail.com>
2015-02-08 16:04:42 -0600

    added time and appropriate greeting message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
   def index
+    @current_time = Time.now.to_formatted_s(:short)
+    # Adjust message depending on the hour of the day!
+    @greet_message = case Time.now.hour
+    when 5..11
+      "Goooooood Morning!"
+    when 12..15
+      "Good afternoon!"
+    when 16..19
+      "Good evening!"
+    when 20..23, 0..4
+      "Good NIGHT"
+    end
+
   end
 end
