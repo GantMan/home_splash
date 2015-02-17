efd44daffe9ddc12f9e2e8d1669efa0fe46cfaaa
Gant <GantMan@gmail.com>
2015-02-16 20:25:52 -0600

    included gem for class and styled the page slightly

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'stamp'
+
 # Easy replacement of Webrick!   Much smaller, nicer, friendlier.
 gem 'thin'
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @current_time = Time.now.to_formatted_s(:short)
+    @current_time = Time.now.stamp_like("Feb 1st 09:00 pm")
     # Adjust message depending on the hour of the day!
     @greet_message = case Time.now.hour
     when 5..11
