b29189f27b2df5209c538c2f30b16d8e78df02f2
Gant <GantMan@gmail.com>
2015-02-18 11:04:32 -0600

    added some seeds for Quote

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Quote.create(quip: 'Wherever you go, there you are!')
+Quote.create(quip: 'Something Witty!')
+Quote.create(quip: 'Second mouse gets the cheese')
+Quote.create(quip: 'Fool-proof should not be taken as a challenge')
