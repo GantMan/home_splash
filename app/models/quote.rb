1fd8967302ad63108471f8a278448ed742980d2f
Gant <GantMan@gmail.com>
2015-03-02 10:29:51 -0600

    added recent scope to Quote

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,6 @@
 class Quote < ActiveRecord::Base
+  validates :quip, presence: true, length: { minimum: 3 }, uniqueness: true
+
+  scope :recent, -> (weeks_past=1) { where("created_at > ?", weeks_past.weeks.ago) }
+
 end
