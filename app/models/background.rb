8f0ae587d17c2413dd3bec9e47c3358a8f5a1673
Gant <GantMan@gmail.com>
2015-03-16 15:26:19 -0500

    fixed bug

--- app/models/background.rb
+++ app/models/background.rb
@@ -1,5 +1,5 @@
 class Background < ActiveRecord::Base
   belongs_to :user
 
-  validates :image_file,, presence: true, uniqueness: {scope: :user_id}, length: {in: 2..255}
+  validates :image_file, presence: true, uniqueness: {scope: :user_id}, length: {in: 2..255}
 end
